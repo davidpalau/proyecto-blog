0a7f9b6a857ceed36209651e38f5c8a971a692f4
davidpalau <palau.david@hotmail.com>
2024-05-12 11:46:27 +0200

    CSS y admin correcto

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
 
             @foreach ($posts as $post)
-              <img class="" src="{{$post->image->url}}" alt="">
+              <img class="" src="http://localhost/proyecto/1_blog/proyecto-blog/public{{ Storage::url($post->image->url) }}"  alt="">
                 <article class="w-full h-full px-8 flex flex-col justify-center">
                     <p>{{($post->name) }} :  {{$post->extract}} <a href="{{ route('posts.show', $post) }}" class="">
                         Leer más</a><br> Etiquetas:
