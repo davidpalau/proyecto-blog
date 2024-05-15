6ec6d9477fdab679a81c2fa29ad3734ec792d5be
davidpalau <palau.david@hotmail.com>
2024-05-15 17:12:12 +0200

    Casi finalizado falta apariencia admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use App\Models\Category;
-use App\Models\Post;
 use App\Models\Tag;
-use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 
@@ -15,22 +11,17 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
-
+    public function run()
     {
-        Storage::makeDirectory('posts');
         Storage::deleteDirectory('posts');
+        Storage::makeDirectory('posts');
+        // $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         Category::factory(4)->create();
         Tag::factory(8)->create();
         $this->call(PostSeeder::class);
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
