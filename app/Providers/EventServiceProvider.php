19554d50ae64192c92f8306e1ecdd3687d79d401
davidpalau <palau.david@hotmail.com>
2024-06-07 20:27:19 +0200

    CRUDs terminados

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Models\Post;
+use App\Observers\PostObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,14 +27,15 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Post::observe(PostObserver::class);
     }
+}
 
     /**
      * Determine if events and listeners should be automatically discovered.
      */
-    public function shouldDiscoverEvents(): bool
-    {
-        return false;
-    }
-}
+//     public function shouldDiscoverEvents(): bool
+//     {
+//         return false;
+//     }
+// }
